42fed3ed0c3196b5573d3499f2eaa38055aed4e6
Irpan <straidr222@gmail.com>
2024-11-09 11:10:23 +0700

    Commit 2

--- android/app/src/main/kotlin/com/example/belajar_theme_builder/MainActivity.kt
+++
@@ -1,5 +0,0 @@
-package com.example.belajar_theme_builder
-
-import io.flutter.embedding.android.FlutterActivity
-
-class MainActivity: FlutterActivity()
--- lib/home/home_binding.dart
+++
@@ -1,9 +0,0 @@
-import 'package:belajar_theme_builder/home/home_viewmodel.dart';
-import 'package:get/get.dart';
-
-class HomeBinding extends Bindings {
-  @override
-  void dependencies() {
-    Get.lazyPut(() => HomeViewmodel());
-  }
-}
